ac8bb2554b02ef4b3c492fef704a79b1ec6f19e2
Momo <amirmomo8@gmail.com>
2026-05-21 11:42:00 +0200

    sets up frontend initial structure

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="main text-primary container my-20 flex flex-col gap-y-20">
+      {" "}
+      <h1 className="hero-header">hellos</h1>
+      <Image src="/next.svg" width={100} height={100} alt="alt" />
+      <div className="bg-accent flex flex-col gap-4 p-4 rounded-lg">
+        <h3 className="text-accentthree ">hellos</h3>
+        <h2 className="text-accentthree">HELLO</h2>
+        <p className="text-accentthree">hellos</p>
+      </div>
+    </main>
+  );
+}
