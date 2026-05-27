d9bf4514e6a1d9f583b96e7287e6c6a49fa8e109
Momo Amir Kruse Amer <maea@kollab.dk>
2026-05-27 11:33:47 +0200

    adds correct routes and moves proxy to src + page routes

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { Register as RegisterComponent } from "@/components/Register";
+
+export default function Register() {
+  return (
+    <div className='text-primary container m-auto'>
+      <RegisterComponent />
+    </div>
+  );
+}
