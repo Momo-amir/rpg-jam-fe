07ac27c17cb655b35db0c1e85b6395c4ab862426
Momo Amir Kruse Amer <maea@kollab.dk>
2026-06-08 10:16:33 +0200

    adds logout functionality + changes some small stuff with card :)

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,16 +1,14 @@
-
-import Image, { StaticImageData } from "next/image"
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { Url } from "next/dist/shared/lib/router/router";
 
 type CardProps = {
-  title: string
-  description: string
-  image:  string | StaticImageData;
-  imageAlt: string
-  href: Url 
-}
-
+  title: string;
+  description: string;
+  image: string | StaticImageData;
+  imageAlt: string;
+  href: Url;
+};
 
 export default function Card({
   title,
@@ -20,18 +18,17 @@ export default function Card({
   href,
 }: CardProps) {
   return (
-    <div>
-      <Link href={href} className="group relative block w-full h-full">
-        <Image
-          src={image}
-          alt={imageAlt}
-          className="rounded-lg group-hover:opacity-65 transition-opacity duration-300"
-        />
-        <div className="absolute inset-0 flex flex-col opacity-0 justify-end p-4 group-hover:opacity-100 transition-opacity duration-300">
-          <h2>{title}</h2>
-          <p>{description}</p>
-        </div>
-      </Link>
-    </div>
+    <Link href={href} className='group relative block w-full overflow-hidden rounded-lg aspect-video md:aspect-3/4'>
+      <Image
+        src={image}
+        alt={imageAlt}
+        fill
+        className='object-cover group-hover:opacity-65 transition-opacity duration-300'
+      />
+      <div className='absolute inset-0 flex flex-col justify-end p-4'>
+        <h2>{title}</h2>
+        <p className='opacity-0 group-hover:opacity-100 transition-opacity duration-300'>{description}</p>
+      </div>
+    </Link>
   );
 }
