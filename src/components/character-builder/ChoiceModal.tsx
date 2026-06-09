80c4a6a1e1ece87a53762ddfa22eeb06cd14f348
Momo Amir Kruse Amer <maea@kollab.dk>
2026-06-09 13:27:38 +0200

    adds type guard since i got owned

--- src/components/character-builder/ChoiceModal.tsx
+++ src/components/character-builder/ChoiceModal.tsx
@@ -23,6 +23,8 @@ export function ChoiceModal({
   selected,
   onConfirm,
 }: ChoiceModalProps) {
+  const selectedMulti = Array.isArray(selected) ? selected : []; // best practice type guard - outer check only makes sure things are truthy this is to make sure we are certain about the type
+  const selectedSingle = typeof selected === "string" ? selected : undefined;
   return (
     <Modal open={open} onOpenChange={onOpenChange} title={title}>
       {options && options.length > 0 && onConfirm ? (
@@ -31,19 +33,21 @@ export function ChoiceModal({
             multiSelect
             count={numberOfChoices}
             options={options}
-            selected={(selected as string[]) ?? []}
+            selected={selectedMulti}
             onConfirm={onConfirm}
           />
         ) : (
           <OptionCardList
             options={options}
-            selected={selected as string | undefined}
+            selected={selectedSingle}
             onConfirm={onConfirm}
           />
         )
       ) : (
         <div className='flex min-h-48 flex-col items-center justify-center'>
-          <p className='text-helper text-neutraltwo/60'>No options available yet.</p>
+          <p className='text-helper text-neutraltwo/60'>
+            No options available yet.
+          </p>
         </div>
       )}
     </Modal>
